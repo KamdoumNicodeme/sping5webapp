49975169c1666ec49fc9485aab10cc21e80ee7da
Nicodeme KAMDOUM <tchietcheujean@yahoo.fr>
2024-05-23 20:42:15 +0200

    first

---
+++ src/test/java/com/nico/sping5webapp/Sping5webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico.sping5webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping5webappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
